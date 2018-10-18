8e494e7b5d494b07e79d72e1dbf4b26f51bf90b4
MasayoshiNomura <masayoshi.nomura@outlook.jp>
2018-10-18 14:06:13 +0900

    this is the first app with laravel.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Develop ACS</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -79,16 +79,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Develop ACS Home
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="http://localhost:8000">Home</a>
+                    <a href="#">News</a>
+                    <a href="#">Documentation</a>
+                    <a href="#">Nova</a>
+                    <a href="#">Forge</a>
+                    <a href="#">GitHub</a>
                 </div>
             </div>
         </div>
